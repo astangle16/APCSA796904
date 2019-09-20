08258baa6fb184976a79f4fe7fb3005acd59cbc7
astangle16 <796904@seq.org>
2019-09-20 10:25:22 -0700

    920

---
+++ LabsSemester1/Lab 920/SentenceBuilder.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class SentenceBuilder here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SentenceBuilder
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SentenceBuilder
+     */
+    public SentenceBuilder()
+    {
+       public void loadSentences()
+
+       String[] nouns  = new String [5];
+       nouns[0] = "person";
+       nouns[1] = "Alec";
+       nouns[2] = "Nathan";
+       nouns[3] = "Justin";
+       nouns[4] = "Peter";
+       
+       Strings[] verbs = {"is", "tired", "ran", "slept", "read", "swim", "behave"} 
+      Strings[] adjectives = "red","Green","awesome","witty","plain","skinny","fit","fancy"}
+       public 
