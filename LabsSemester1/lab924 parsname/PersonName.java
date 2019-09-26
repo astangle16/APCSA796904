cf5c4dc5e0eaf3e314098bcdea2e285373c9b954
astangle16 <796904@seq.org>
2019-09-26 10:32:02 -0700

    926

--- LabsSemester1/lab924 parsname/PersonName.java
+++ LabsSemester1/lab924 parsname/PersonName.java
@@ -2,49 +2,58 @@ public class PersonName
 {
 
     public String Fname="", Mname="", Lname="";
-    
 
     public PersonName() {
-    
     }
-    
+
     public PersonName(String fullName){
         parseName(fullName);
-    
+
     }
-    
-    
+
     public void parseName(String fullName)
     {
         System.out.println("Welocome to ParseLand..." + fullName);
-        
-        // Format One:  	"Last, First Middle"
-        if(fullName.contains(",") && !fullName.matches("\\S+")) {
-            System.out.println("FORMAT 1");
 
-            int commaLocation = fullName.indexOf(",");
-            Lname = fullName.substring(0,commaLocation);
-            
+        // Format One:      "Last, First Middle"
+        // if(fullName.contains(",") && !fullName.matches("\\S+")) {
+
+        int commaLocation = fullName.indexOf(",");
+        if(commaLocation != -1 ) {
             String restOfName = fullName.substring(commaLocation+2, fullName.length());
             System.out.println("restOfName=" + restOfName);
             int spaceLocation = restOfName.indexOf(" ");
-            Fname = restOfName.substring(0, spaceLocation);
-           
-            restOfName = restOfName.substring(spaceLocation+1, restOfName.length());
-            System.out.println("restOfName=" + restOfName);
-            Mname = restOfName.substring(0, restOfName.length());
+            if(spaceLocation != -1) {
+                System.out.println("FORMAT 1");
 
+                Fname = restOfName.substring(0, spaceLocation);
+                Mname = restOfName.substring(spaceLocation+1, restOfName.length());
+                Lname = fullName.substring(0,commaLocation);
+                System.out.println("Fname..." + Fname);
+                System.out.println("Mname..." + Mname); 
+                System.out.println("Lname..." + Lname);
+
+            }   
         }
-        
-        //Format Two:  	"Last, First"
-         
-        // Format Three:  	"First Middle Last"
-            
-        //  Format four:  	"First Last"
-        
-        // log
-       System.out.println("Fname..." + Fname);
-       System.out.println("Mname..." + Mname); 
-       System.out.println("Lname..." + Lname);
+
+        // String restOfName = fullName.substring(commaLocation+2, fullName.length());
+        //System.out.println("restOfName=" + restOfName);
+        //int spaceLocation = restOfName.indexOf(" ");
+        //Fname = restOfName.substring(0, spaceLocation);
+
+        // restOfName = restOfName.substring(spaceLocation+1, restOfName.length());
+        // System.out.println("restOfName=" + restOfName);
+        // Mname = restOfName.substring(0, restOfName.length());
+
     }
+
+    //Format Two:   "Last, First"
+
+    // Format Three:    "First Middle Last"
+
+    //  Format four:    "First Last"
+
+    // log
+
 }
+
