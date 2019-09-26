ecfb8266c70b8da506bcea1f8f8224a241cff87d
astangle16 <796904@seq.org>
2019-09-25 23:21:33 -0700

    Update PersonRunner.java

--- LabsSemester1/lab924 parsname/PersonRunner.java
+++ LabsSemester1/lab924 parsname/PersonRunner.java
@@ -1,30 +1,21 @@
-
-/**
- * Scanner tester
- *
- * @author (eettlin)
- * @version (924)
- */
-
 import java.util.Scanner;
 public class PersonRunner{
-    // Scanner kb = new Scanner3                
+    
    
-    Scanner kb = new Scanner(System.in);
-    string.inputstr = "";
     public static void main(){
+    
         Scanner kb = new Scanner(System.in);
         String inputStr = "";
-        //  if(str1 == str2)
         
         while(!inputStr.equals("quit")){
+            
             System.out.println("Enter a name or type \"quit\" to quit");
             inputStr = kb.nextLine();
             System.out.println(inputStr);
-            Person per = new Person(inputstr);
+            PersonName per = new PersonName(inputStr);
             
         }
-        System.out.print
+        //System.out.print
         
     }
 }
