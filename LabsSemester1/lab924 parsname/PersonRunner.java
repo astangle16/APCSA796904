aeb99f6cc2e434abd035bae92238d5afaf234f05
astangle16 <796904@seq.org>
2019-10-08 13:37:49 -0700

    gh

--- LabsSemester1/lab924 parsname/PersonRunner.java
+++ LabsSemester1/lab924 parsname/PersonRunner.java
@@ -1,40 +1,41 @@
 import java.io.*;
 import java.util.*;
 
-
-
 /*
  * SOLUTION
  */
 
-  
-  
-  
-  /*
-   * RUNNER
-   */
-  public  class PersonRunner {
-      
-      // main
-  public static void main(String[] args) {  
-      PersonRunner per = new PersonRunner();
-  }
-  
+/*
+ * RUNNER
+ */
+public  class PersonRunner {
+
+    // main
+    public static void main(String[] args) {  
+        PersonRunner per = new PersonRunner();
+    }
+
     public PersonRunner() {
-      
+
         Scanner kb = new Scanner(System.in);
         String inputStr = "";
-        
+
         while(!inputStr.equals("quit")){
-            
+
             System.out.println("Enter a name or type \"quit\" to quit");
             inputStr = kb.nextLine();
             // System.out.println(inputStr);
             PersonName per = new PersonName(inputStr);
-            
-        }
 
+        }
+    }
+
+    public Person[] extendArray(Person[] p1, int i){
+        Person[] s2 = new Person[p1.length+i];
+
+        for(int i = 0; i <p1.length; i++){
+            p2[i] = p1[i];
+        }
     }
 }
 
- 
