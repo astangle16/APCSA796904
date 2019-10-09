94e8b2089961dbad3e4a3f0d9c2b42464e9e9b49
astangle16 <796904@seq.org>
2019-10-09 13:59:09 -0700

    bcdg

--- LabsSemester1/lab924 parsname/PersonRunner.java
+++ LabsSemester1/lab924 parsname/PersonRunner.java
@@ -30,12 +30,37 @@ public  class PersonRunner {
         }
     }
 
+    
     public Person[] extendArray(Person[] p1, int i){
+        
+        
+        
         Person[] s2 = new Person[p1.length+i];
 
         for(int i = 0; i <p1.length; i++){
             p2[i] = p1[i];
         }
+        
+        public static void main(){
+            Person[] persons = new Persons[2];
+            int count = 0;
+            
+            Scanner kb = new Scanner(System.in);
+            System.out.println("enter a name");
+            String kbInput = kb.nextLine();
+            
+            while(kbInput.equals(
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
