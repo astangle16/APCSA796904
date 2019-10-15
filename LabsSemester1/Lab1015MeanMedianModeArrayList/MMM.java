83fef11bba7c6637e5f9c16bf176cfa3702e9a65
astangle16 <796904@seq.org>
2019-10-15 10:43:41 -0700

    blee blah blah blah blu blah

---
+++ LabsSemester1/Lab1015MeanMedianModeArrayList/MMM.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class MMM here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+public class MMM
+{
+    
+    private ArrayList<Integer> nums;
+    
+    public MMM()
+    {
+        nums = new ArrayList<Integer>();
+        laodArray();
+        findMean();
+        findMode();
+        
+    }
+    
+    
+    public static void main(){
+        MMM mmm = new MMM();
+        
+    }
+    
+    loadArray loadarray = new LoadArray();
+
+    
+}
