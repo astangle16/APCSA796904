7e6dd6da8b9150add349708b293c4e4093670d6f
astangle16 <796904@seq.org>
2019-11-21 16:37:20 -0800

    Update Student.java

--- ProjectsSemester1/StudentList/Student.java
+++ ProjectsSemester1/StudentList/Student.java
@@ -1,4 +1,3 @@
-
 /**
  * Write a description of class Student here.
  *
@@ -8,72 +7,72 @@
 public class Student
 {
     // instance variables - replace the example below with your own
-     private int stuNum;
-     private double gpa;
-     private String fName = "";
-     private String mName = "";
-     private String lName = "";
-     
-     
+    private int stuNum;
+    private double gpa;
+    private String fName = "";
+    private String mName = "";
+    private String lName = "";
+
     public Student(String fullName,int stuNum,double gpa){
-       this.stuNum = stuNum;
-       this.gpa = gpa;
-       parseName(fullName);
+        this.stuNum = stuNum;
+        this.gpa = gpa;
+        parseName(fullName);
     }
 
-   
     public void parseName(String fullName)    {
+        int fsi = fullName.indexOf(" "); // first space index
+        int lsi = fullName.lastIndexOf(" "); // last space index
+        int ci =  fullName.indexOf(","); // comma index
 
-      int fsi = fullName.indexOf(" "); // first space index
-      int lsi = fullName.lastIndexOf(" "); // last space index
-      int ci =  fullName.indexOf(","); // comma index
-      
-      // System.out.println("index_of_first_space= " + index_of_first_space);
-      // System.out.println("index_of_second_space= " + index_of_second_space);
-      // System.out.println("index_of_comma= " + index_of_comma);
+        // System.out.println("index_of_first_space= " + index_of_first_space);
+        // System.out.println("index_of_second_space= " + index_of_second_space);
+        // System.out.println("index_of_comma= " + index_of_comma);
 
-         
-      if(ci != -1){ // comma 
+        if(ci != -1){ // comma 
+            if(fsi != lsi) { // two spaces
+                System.out.println("comma two spaces");              
+                // Format Three:    "First, Middle Last"
+                fName = fullName.substring(0, fsi-1);
+                mName = fullName.substring(fsi+1, lsi);
+                lName = fullName.substring(lsi, fullName.length());
 
-        if(fsi != lsi) { // two spaces
-          
-          System.out.println("comma two spaces");              
-          // Format Three:    "First, Middle Last"
-          fName = fullName.substring(0, fsi-1);
-          mName = fullName.substring(fsi+1, lsi);
-          lName = fullName.substring(lsi, fullName.length());
+            } else { // one space
 
-        } else { // one space
-          
-         System.out.println("comma one space");             
-         fName = fullName.substring(lsi+1, fullName.length());
-         
-         lName = fullName.substring(0, ci);
-        }
-        
-      } else {  // no comma
+                System.out.println("comma one space");             
+                fName = fullName.substring(lsi+1, fullName.length());
 
-        if(fsi != lsi) { // two spaces
-          fName = fullName.substring(0, fsi);
-          mName = fullName.substring(fsi, lsi );
-          lName = fullName.substring(lsi, fullName.length());
-          System.out.println("no comma and two spaces");             
-          
-          
-        } else { // one space
-          fName = fullName.substring(0, fsi);
-          mName = fullName.substring(fsi, lsi );
-          lName = fullName.substring(lsi, fullName.length());
-          System.out.println("no comma and one spaces");             
+                lName = fullName.substring(0, ci);
+            }
+
+        } else {  // no comma
+
+            if(fsi != lsi) { // two spaces
+                fName = fullName.substring(0, fsi);
+                mName = fullName.substring(fsi, lsi );
+                lName = fullName.substring(lsi, fullName.length());
+                System.out.println("no comma and two spaces");             
+
+            } else { // one space
+                fName = fullName.substring(0, fsi);
+                mName = fullName.substring(fsi, lsi );
+                lName = fullName.substring(lsi, fullName.length());
+                System.out.println("no comma and one spaces");             
+            }
+
         }
 
-      }
-      
-      System.out.println("fName=" + fName);
-       System.out.println("mName=" + mName);
-      System.out.println("lName=" + lName);
+        System.out.println("fName=" + fName);
+        System.out.println("mName=" + mName);
+        System.out.println("lName=" + lName);
 
-      
-      
     }
-  }
+    public int getStuNum(){
+        return stuNum;
+    }
+    public String getLastName(){
+        return lName;
+    }
+    public double getGPA(){
+    return gpa;
+    }
+}
