71f12bc7443e3186b6fc33ed39f28e7462f5ead2
astangle16 <796904@seq.org>
2019-12-02 13:30:08 -0800

    drgfdf

--- LabsSemester1/YOUTUBE_CODE/video13While.java
+++ LabsSemester1/YOUTUBE_CODE/video13While.java
@@ -5,18 +5,18 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class video13
+public class video13While
 {
     // instance variables - replace the example below with your own
     private int x;
 
 
-    public static void main (String args[]){
+    public static void main (){
         {
             int counter = 0;
             while(counter < 10){
                 System.out.println(counter);
-                counter++;
+                counter+= 2;
             }
         }
     }
