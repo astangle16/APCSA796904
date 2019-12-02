85b0552b474f49ddf66b102b5f9dae119522530f
astangle16 <796904@seq.org>
2019-12-02 13:09:18 -0800

    1202

---
+++ LabsSemester1/YOUTUBE_CODE/video13.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class video13 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class video13
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+
+    public static void main (String args[]){
+        {
+            int counter = 0;
+            while(counter < 10){
+                System.out.println(counter);
+                counter++;
+            }
+        }
+    }
+}
