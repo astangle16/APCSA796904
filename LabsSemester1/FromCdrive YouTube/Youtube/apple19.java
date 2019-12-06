3b178f30a6c1b892b0472ff1e518feac33ed5eb4
Alec Rod <arod@WS-ACADBUS.EDU>
2019-12-06 13:29:53 -0800

    vhn

---
+++ LabsSemester1/FromCdrive YouTube/Youtube/apple19.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class apple19 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class apple19
+{
+   public static void main(String[]args){
+   int age = 55;
+   
+   if(age > 60)
+   System.out.println("you are a senior sitizen");
+   
+   else if(age >= 50)
+   System.out.println("you are in your 50's");
+   else if(age >= 40)
+   System.out.println("you are in your 40's");
+   else
+   System.out.println("you are young");
+   
+       
+       
+    }
+}
