217cb935fec81762206e1d267bd09e9ba0d52a2d
Alec Rod <arod@WS-ACADBUS.EDU>
2019-12-09 14:34:35 -0800

    gfhd

---
+++ LabsSemester1/FromCdrive YouTube/Youtube/apple20.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class apple20 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class apple20
+{
+    public static void main(String[] args){
+    int age = 21;
+    System.out.println(age > 50 ? "you are old": "you are young");
+    }
+}
---
+++ LabsSemester1/FromCdrive YouTube/Youtube/apple21.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+/**
+ * Write a description of class apple21 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class apple21
+{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        int total = 0;
+        int grade;
+        int average;
+        int counter = 0;
+        
+    }
+    
+  
+    public apple21()
+    {
+        
+    }
+
+ 
+    public int sampleMethod(int y)
+    {
+        
+    }
+}
